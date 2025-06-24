fdd474444d9e9fcea98dd4cb4b8849535a1bdd53
Jean Marie Vianey RABEMANALINA <vianey.jean@ymail.com>
2025-06-24 09:52:13 +0400

    bug

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,8 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import DashboardPage from "./pages/DashboardPage";
-import NotFoundPage from "./pages/NotFound";
+import NotFoundPage from './pages/NotFound';
+
 
 // Création d'un client React Query pour gérer les requêtes réseau, le cache, etc.
 const queryClient = new QueryClient();
